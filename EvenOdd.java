010ef14f36e508a909ac4a04df7acd73d494ce3d
Gloria <mutuyegloria@gmail.com>
2025-03-27 05:00:18 -0700

    second commit

--- EvenOdd.java
+++ EvenOdd.java
@@ -2,10 +2,10 @@ public class EvenOdd {
     public static void main(String[] args) {
         int a=50;
         for (int i=0; i<=50;i++){
-            if(a%i==0){
-              System.out.println("the number us Even"+a);  
+            if(i%2==0){
+              System.out.println("the number us Even"+i);  
             }else {
-                System.out.println("the number is odd"+a);
+                System.out.println("the number is odd"+i);
             }
         }
        
